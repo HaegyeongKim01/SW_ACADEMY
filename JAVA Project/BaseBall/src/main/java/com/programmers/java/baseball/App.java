4412797d6fb9120365c59432ca689ebc58d6b720
haegyeong <haegyeong.kim01@gmail.com>
2023-01-13 14:19:44 +0900

    Modified BallCount

--- JAVA Project/BaseBall/src/main/java/com/programmers/java/baseball/App.java
+++ JAVA Project/BaseBall/src/main/java/com/programmers/java/baseball/App.java
@@ -2,6 +2,6 @@ package com.programmers.java.baseball;
 
 public class App {
     public static void main(String[] args) {
-
+        System.out.println("d");
     }
 }
--- JAVA Project/BaseBall/src/main/java/com/programmers/java/baseball/engine/BaseBall.java
+++
@@ -1,4 +0,0 @@
-package com.programmers.java.baseball.engine;
-
-public class BaseBall {
-}
